f57ddd1933efde5fbecd13a38bba7da82d427d86
Tanvi Garg <tanvi.garg23@gmail.com>
2019-01-06 22:42:08 -0800

    add functions to count vowels, consonants and just the first 8 words in first sentence

--- src/entry.js
+++ src/entry.js
@@ -1,10 +1,31 @@
-export function Entry(date, title, entry) {
+export function Entry(date, title, journalEntry) {
   this.date = date;
   this.title = title;
   this.journalEntry = journalEntry;
 }
 
 Entry.prototype.wordInSentenceCount = function() {
-  var words = this.journalEntry.split(" ");
+  let words = this.journalEntry.split(" ");
   return words.length;
+};
+
+Entry.prototype.vowelsInSentenceCount = function() {
+  let vowels = this.journalEntry.match(/[aeiouAEIOU]/gi);
+  return vowels === null ? 0 : vowels.length;
+};
+
+Entry.prototype.consonantsInSentenceCount = function() {
+  let vowels = this.journalEntry.match(/[^aeiouAEIOU *.!%@&#123456789]/gi);
+  return vowels === null ? 0 : vowels.length;
+};
+
+Entry.prototype.getTeaser= function() {
+  let sentencesAll = this.journalEntry.split('. ');
+  let wordsFirstSentence = sentencesAll[0].split(' ');
+  let teaserWords = [];
+  for (let i = 0; i < 8; i++) {
+    teaserWords.push(wordsFirstSentence[i]);
+  }
+  var teaser = teaserWords.join(' ');
+  return teaser;
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+import { Entry } from './entry';
 import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  console.log("some text!");
+  $('#form').submit(function(event) {
+      event.preventDefault();
+      let date = $('#date').val();
+      let title = $('#title').val();
+      let entry = $('#entry').val();
+      let newEntry = new Entry(date, title, journalEntry);
 });
